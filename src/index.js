f31f1adccf726c0d7aaa004cbd3d87d91439e38e
Phan Quân <qmp23@drexel.edu>
2020-11-08 13:37:35 -0500

    Correct import dir for Board in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Board from './Components/Board'
+import Board from './components/Board'
 
 class Game extends React.Component {
   render() {

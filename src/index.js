beb3610fffccb190922c9a4786e52b6d0e0b31ee
Phan Quân <qmp23@drexel.edu>
2020-11-08 23:48:07 -0500

    Update directory conflicts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Board from './components/Board'
+import Board from './components/Board/Board'
 
 class Game extends React.Component {
   render() {
